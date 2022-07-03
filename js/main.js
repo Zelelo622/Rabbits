803bd4c6ff8a372e0f8dba6c5b029dd0b8bb7ef8
Egor Kirin <egorcom27@gmail.com>
2022-07-03 20:05:32 +0300

    dropdown

--- js/main.js
+++ js/main.js
@@ -14,3 +14,61 @@ btnsTop.forEach((btn, index) => btn.addEventListener('click', () => {
     hideActiveBtn();
     activateBtn(index);
 }));
+
+// =========DROPDOWN=========
+
+function showList() {
+    const menu = document.querySelector('.dropdown-menu');
+
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+// function toggleClass(elem, className) {
+//     if (elem.className.indexOf(className) !== -1) {
+//         elem.className = elem.className.replace(className, '');
+//     }
+//     else {
+//         elem.className = elem.className.replace(/\s+/g, ' ') + className;
+//     }
+
+//     return elem;
+// }
+
+// function toggleDisplay(elem) {
+//     const curDisplayStyle = elem.style.display;
+
+//     if (curDisplayStyle === 'none' || curDisplayStyle === '') {
+//         elem.style.display = 'block';
+//     }
+//     else {
+//         elem.style.display = 'none';
+//     }
+// }
+
+
+// function toggleMenuDisplay(e) {
+//     const dropdown = e.currentTarget.parentNode;
+//     const menu = dropdown.querySelector('.dropdown-menu');
+
+//     toggleClass(menu, 'hide');
+// }
+
+// function handleOptionSelected(e) {
+//     toggleClass(e.target.parentNode, 'hide');
+
+//     const id = e.target.id;
+//     const newValue = e.target.textContent + ' ';
+//     const titleElem = document.querySelector('.dropdown-toggle');
+//     titleElem.textContent = newValue;
+// }
+
+// const dropdownTitle = document.querySelector('.dropdown-toggle');
+// const dropdownOptions = document.querySelectorAll('.dropdown-item');
+
+//bind listeners to these elements
+// dropdownTitle.addEventListener('click', toggleMenuDisplay);
+// dropdownOptions.forEach(option => option.addEventListener('click', handleOptionSelected));
